421d1cf1e0d9989e274343875bf84bd33aa08bc8
lhj707 <153067233+lhj707@users.noreply.github.com>
2023-12-06 17:36:03 +0900

    Update myGPT.py

--- myGPT.py
+++ myGPT.py
@@ -1,17 +1,3 @@
-pip install streamlit
-pip install tiktoken
-pip install pysqlite3-binary
-pip install langchain
-pip install chromadb
-pip install unstructured
-pip install sentence-transformers
-pip install faiss-cpu
-pip install tiktoken
-pip install openai
-pip install pypdf
-pip install loguru
-pip install docx2txt
-
 import streamlit as st
 import tiktoken
 from loguru import logger
