55ff515c14793e94062bf96811649db94c3dcb0d
lhj707 <153067233+lhj707@users.noreply.github.com>
2023-12-06 20:05:28 +0900

    Update myGPT.py

--- myGPT.py
+++ myGPT.py
@@ -37,7 +37,7 @@ def main():
 
     with st.sidebar:
         uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        #openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
     if process:
         if not openai_api_key:
@@ -137,7 +137,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
+    llm = ChatOpenAI(openai_api_key=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, model_name = 'gpt-3.5-turbo',temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
