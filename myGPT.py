5af31223aa9430b3ca796fa8d21c41c844529293
lhj707 <153067233+lhj707@users.noreply.github.com>
2024-01-18 13:51:47 +0900

    Update myGPT.py

--- myGPT.py
+++ myGPT.py
@@ -124,8 +124,8 @@ def get_text(docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=100,
-        chunk_overlap=10,
+        chunk_size=900,
+        chunk_overlap=100,
         length_function=tiktoken_len
     )
     chunks = text_splitter.split_documents(text)
